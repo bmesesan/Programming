5931efb0196f8128500c4e5f88355fc81de46ee9
bmesesan <mesesan.boby@yahoo.com>
2022-12-30 16:56:39 +0200

    Merge sort, reverse sublist

--- 01-linked_list/1_linked_list.cpp
+++ 01-linked_list/1_linked_list.cpp
@@ -12,126 +12,371 @@ class Node
         int data;
         Node *next;
 
-        Node(int value){
+        Node(int value) {
             data = value;
             next = NULL;
         }
 };
 
-// 1) Display elements of linked list
-void display(Node *head)
+class LinkedList
 {
-   Node *curr = head;
-   while(curr != NULL) {
-       printf("%d ", curr->data);
-       curr = curr->next;
-   }
+    private:
+        Node *m_head;
+        int m_len = 0;
+    
+    public:
+        LinkedList(void)
+        {
+            m_head = NULL;
+        }
+
+        void setHead(Node *f_head)
+        {
+            m_head = f_head;
+        }
+
+        Node* getHead(void)
+        {
+            return m_head;   
+        }
+
+        // 1) Display elements of linked list
+        void display(void)
+        {
+            Node *curr = m_head;
+            while(curr != NULL) {
+                printf("%d ", curr->data);
+                curr = curr->next;
+            }
+        }
+
+        // 2.1) Insert element at the begining of the list
+        void insertAtHead(int new_data)
+        {
+            // Allocate memory for the new node
+            Node *new_node = new Node(new_data);
+
+            // Set the next reference of the new node as the head
+            new_node->next = m_head;
+
+            // Set the new node as head
+            m_head = new_node;
+
+            m_len += 1;
+        }
+
+        // 2.2) Insert element after a known element in the list
+        void insertAfter(Node* prev_node, int new_data)
+        {
+            // Check if the given previous node is NULL or not
+            if (prev_node == NULL) {
+                printf("Prev node can not be NULL!\n");
+                return;
+            }
+
+            // Allocate a new node
+            Node *new_node = new Node(new_data);
+
+            // Make the next of new node as the next of previous node
+            new_node->next = prev_node->next;
+
+            // Finally, move the next of the previous node as a new node
+            prev_node->next = new_node;
+
+            m_len += 1;
+        }
+
+        // 2.3) Add an element at the end of the list
+        void append(int new_data)
+        {
+            // Allocate a new node
+            Node *new_node = (Node*)malloc(sizeof(Node));
+            Node *last = m_head;
+
+            // Increment the list size
+            m_len += 1;
+
+            // 2. Put in the data
+            new_node->data = new_data;
+
+            // 3. This new node is going to be the last node, so make next of it as NULL
+            new_node->next = NULL;
+
+            // 4. If the Linked List is empty, then make the new node as head
+            if (m_head == NULL) { 
+                m_head = new_node; 
+                return; 
+            }
+
+            // 5. Else find the last node
+            while (last->next != NULL){
+                last = last->next;
+            }
+
+            // Put the new node at the end of the list
+            last->next = new_node;
+        }
+
+        // 3.1) Delete from Beginning
+        void deleteFirst(void)
+        {
+            Node *temp = m_head;
+
+            if(m_len == 0) {
+                printf("List is empty!\n");
+                return;
+            }
+
+            m_head = m_head->next;
+            free(temp);
+
+            m_len -= 1;
+        }
+
+        // 3.2) Delete from End
+        void deleteLast(void)
+        {
+            Node *end = m_head;
+            Node *prev = NULL;
+
+            if(m_len == 0) {
+                printf("List is empty!\n");
+                return;
+            }
+
+            // Set the last node and the previous to last node
+            while(end->next) {
+                prev = end;
+                end = end->next;
+            }
+            // Set the previous to last to be the end node
+            prev->next = NULL;
+
+            // Free unused memory
+            free(end);
+
+            m_len -= 1;
+        }
+
+        // 3.2) Delete from a given position
+        void deletePosition(int f_position)
+        {
+            Node *node = m_head;
+            Node *prev = NULL;
+
+            if(m_len == 0) {
+                printf("List is empty!\n");
+                return;
+            }
+
+            if(f_position >= m_len) {
+                printf("Position to delete: %d is greater than the length of list: %d\n", f_position, m_len);
+                return;
+            }
+
+            if (f_position == 0) {
+                deleteFirst();
+                return;
+            }
+
+            for(int i = 0; i < f_position; i++) {
+                prev = node;
+                node = node->next;
+            }
+
+            prev->next = node->next;
+            free(node);
+            m_len -= 1;
+        }
+
+        void swap(Node** a, Node** b)
+        {
+            Node *aux = *a;
+            *a = *b;
+            *b = aux;
+        }
+
+        // 4.1) Swap 2 keys, given that each node has a unique key
+        void swapNodes(int f_key_1, int f_key_2)
+        {
+            Node **headref = &m_head;
+            Node **n1 = NULL;
+            Node **n2 = NULL;
+
+            while(*headref) {
+                if ((*headref)->data == f_key_1) {
+                    n1 = headref;
+                }
+                else if ((*headref)->data == f_key_2) {
+                    n2 = headref;
+                }
+                headref = &((*headref)->next);
+            }
+
+            if (n1 && n2) {
+                swap(n1, n2);
+                swap(&(*n1)->next, &(*n2)->next);
+            }
+        }
+
+        // 4.2) Reverse a linked list
+        void reverse(void)
+        {
+            Node *curr = m_head;
+            Node *next = NULL;
+            Node *prev = NULL;
+            while (curr) {
+                // Store next
+                next = curr->next;
+                // Reverse current node's pointer  
+                curr->next = prev;
+                // Move pointers one position ahead.
+                prev = curr;
+                curr = next;
+            }
+            m_head = prev;
+        }
+
+        // 7) Reverse a Linked List in groups of given size
+        void reverseSubList(int f_size)
+        {
+            int count = 0;
+            Node *headRef = m_head;
+            Node *curr = NULL;
+            Node *next = NULL;
+            Node *prev = NULL;
+
+            // Return if the size of the sublist is smaller or equal to 1
+            if (f_size <= 1) {
+                return;
+            }
+
+            // Start withe the head iterate through the sublist
+            curr = headRef;
+            while (count < f_size && curr != NULL) {
+                // Each current node will set the next one as the previous node
+                next = curr->next;
+                curr->next = prev;
+                // The new previous node will be the current node
+                // The new current node will be the next
+                // Increment the counter
+                prev = curr;
+                curr = next;
+                count++;
+            }
+
+            // The last node of the reversed sub-list will be the start node == headRef
+            // When we are finished, the last node of the reversed sub-list will point to NULL
+            // Make the last node in the sublist point to the next node of the iteration
+            // Change the head ref to be the last node which changed its next reference
+            headRef->next = next;
+            m_head = prev;
+        }
+};
+
+// 5) Sorted mergew of two linked lists
+LinkedList* sortedMerge(LinkedList f_l1, LinkedList f_l2)
+{
+    LinkedList *new_list = new LinkedList();
+
+    Node *node_1 = f_l1.getHead();
+    Node *node_2 = f_l2.getHead();
+
+    while(node_1 || node_2) {
+        if (node_1 && node_2) {
+            if (node_1->data < node_2->data) {
+                new_list->append(node_1->data);
+                node_1 = node_1->next;
+            } else {
+                new_list->append(node_2->data);
+                node_2 = node_2->next;
+            }
+        } else {
+            if (node_1) {
+                new_list->append(node_1->data);
+                node_1 = node_1->next;
+            } else if (node_2) {
+                new_list->append(node_2->data);
+                node_2 = node_2->next;
+            }
+        }
+    }
+
+    return new_list;
 }
 
-// 2.1) Insert element at the begining of the list
-void insertAtHead(Node** head_ref, int new_data)
+// 6) Utility funciton: splits the source list into front and back lists
+void frontBackSplit(LinkedList* f_sourceList, LinkedList* f_frontList, LinkedList* f_backList)
 {
-    // Allocate memory for the new node
-    Node *new_node = new Node(new_data);
-
-    // Set the next reference of the new node as the head
-    new_node->next = *head_ref;
-
-    // Set the new node as head
-    *head_ref = new_node;
+    Node* sourceHead = f_sourceList->getHead();
+    Node* fast;
+    Node* slow;
+    slow = sourceHead;
+    fast = sourceHead->next;
+ 
+    // Advance 'fast' two nodes, and advance 'slow' one node
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+ 
+    // 'slow' is before the midpoint in the list, so split it in two at that point.
+    f_frontList->setHead(sourceHead);
+    f_backList->setHead(slow->next);
+    slow->next = NULL;
 }
 
-// 2.2) Insert element after a known element in the list
-void insertAfter(Node* prev_node, int new_data)
+// 6) Merge sort of a linked list
+void mergeSort(LinkedList **f_list)
 {
-    // Check if the given previous node is NULL or not
-    if (prev_node == NULL){
-        printf("Prev node can not be NULL!\n");
+    Node *headRef = (*f_list)->getHead();
+    LinkedList *frontList = new LinkedList();
+    LinkedList *backList = new LinkedList();
+
+    // 1) Test if the list is empty or if the length of the list is 1
+    if (!headRef || !headRef->next) {
         return;
     }
 
-    // Allocate a new node
-    Node *new_node = new Node(new_data);
+    // 2) Split the source list into front and back lists
+    frontBackSplit(*f_list, frontList, backList);
 
-    // Make the next of new node as the next of previous node
-    new_node->next = prev_node->next;
+    // 3) Recursively sort the sublists
+    mergeSort(&frontList);
+    mergeSort(&backList);
 
-    // Finally, move the next of the previous node as a new node
-    prev_node->next = new_node;
-}
-
-// 2.3) Add an element at the end of the list
-void append(Node** head_ref, int new_data)
-{
-    // Allocate a new node
-    Node *new_node = (Node*)malloc(sizeof(Node));
-    Node *last = *head_ref;
-
-    // 2. Put in the data
-    new_node->data = new_data;
-
-    // 3. This new node is going to be the last node, so make next of it as NULL
-    new_node->next = NULL;
-
-    // 4. If the Linked List is empty, then make the new node as head
-    if (*head_ref == NULL){ 
-        *head_ref = new_node; 
-        return; 
-    }
-
-    // 5. Else find the last node
-    while (last->next != NULL){
-        last = last->next;
-    }
-
-    // Put the new node at the end of the list
-    last->next = new_node;
-}
-
-// 3.1) Delete from Beginning
-void deleteFirst(Node** head_ref)
-{
-    Node *temp = *head_ref;
-    *head_ref = (*head_ref)->next;
-
-    free(temp);
-}
-
-// 3.2) Delete from End
-void deleteLast(Node** head_ref)
-{
-    Node *end = *head_ref;
-    Node *prev = NULL;
-
+    // 4) Merge the two sorted lists together
+    *f_list = sortedMerge(*frontList, *backList);
+    free(frontList);
+    free(backList);
 }
 
 // Driver code
 int main()
 {
     // Start with the empty list
-    Node* head = NULL;
-     
-    // Insert 6. So linked list becomes 6->NULL
-    append(&head, 6);
-     
-    // Insert 7 at the beginning.
-    // So linked list becomes 7->6->NULL
-    insertAtHead(&head, 7);
-     
-    // Insert 1 at the beginning.
-    // So linked list becomes 1->7->6->NULL
-    insertAtHead(&head, 1);
-     
-    // Insert 4 at the end. So
-    // linked list becomes 1->7->6->4->NULL
-    append(&head, 4);
-     
-    // Insert 8, after 7. So linked
-    // list becomes 1->7->8->6->4->NULL
-    insertAfter(head->next, 8);
-     
-    cout<<"Created Linked list is: ";
-    display(head);
-     
+    LinkedList *my_list_1 = new LinkedList();
+
+    my_list_1->append(6);
+    my_list_1->append(5);
+    my_list_1->append(4);
+    my_list_1->append(3);
+    my_list_1->append(2);
+    my_list_1->append(1);
+    my_list_1->append(0);
+
+    std::cout << "Created Linked list is: " << std::endl;
+    my_list_1->display();
+    std::cout << std::endl;
+
+    my_list_1->reverseSubList(6);
+    std::cout << "Reversed Linked list is: " << std::endl;
+    my_list_1->display();
+    std::cout << std::endl;
+
+    free(my_list_1);
     return 0;
 }
