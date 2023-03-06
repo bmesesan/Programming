9d8d468209b7105cea4695259383a2c3e3723b1d
Mesesan Bogdan (RBRO/ESV-B) <meb1clj@bosch.com>
2023-03-06 21:28:12 +0200

    Eval expression with stack is OK

--- 04-stack/stack.cpp
+++ 04-stack/stack.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <map>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -16,13 +18,66 @@ void reverseString(string &f_str)
     }
 }
 
-// Driver code
-int main()
+void testReverseString(void)
 {
-    stack<char>s;
     string str = "Test Bogdan";
     cout << "String is: " << str << std::endl;
     reverseString(str);
     cout << "Reversed string is : " << str << std::endl;
+}
+
+bool charIsKey(char f_char, const std::map<char, char>& f_map)
+{
+    for (auto iter = f_map.begin(); iter != f_map.end(); iter++) {
+        char current = iter->first;
+        if (f_char == iter->first) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool evalExpression(string &f_expr)
+{
+    stack<char> s;
+    std::map<char, char> m{{'{', '}'}, {'[', ']'}, {'(', ')'}};
+
+    for (int i = 0; i < f_expr.length(); i++) {
+        if (charIsKey(f_expr[i], m)) {
+            s.push(f_expr[i]);
+        } else {
+            char topChr = s.top();
+            if (m[topChr] != f_expr[i]) {
+                return false;
+            }
+            s.pop();
+        }
+    }
+    return true;
+}
+
+void testEvalExpression(void)
+{
+    string expr = "[()]{}{[()()]()}";
+    bool isBalanced = evalExpression(expr);
+    if (isBalanced) {
+        std::cout << "Balanced" << std::endl;
+    } else {
+        std::cout << "Not Balanced " << std::endl;
+    }
+
+    expr = "[(])";
+    isBalanced = evalExpression(expr);
+    if (isBalanced) {
+        std::cout << "Balanced" << std::endl;
+    } else {
+        std::cout << "Not Balanced " << std::endl;
+    }
+}
+
+// Driver code
+int main()
+{
+    testEvalExpression();
     return 0;
 }
