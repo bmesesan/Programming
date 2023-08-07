d1e605cd3a4c4dbeef7a23a21192b5528298d76e
bmesesan <mesesan.boby@yahoo.com>
2023-08-07 17:11:36 +0300

    Symmetrical tree is done.

--- algo-expert/03-binary_trees/09-symmetrical_tree/symmetrical_tree.cpp
+++ algo-expert/03-binary_trees/09-symmetrical_tree/symmetrical_tree.cpp
@@ -18,39 +18,85 @@ class BinaryTree {
         BinaryTree(int value) { this->value = value; }
 };
 
+void computeLeftSub(BinaryTree* tree, vector<int>& leftSub) {
+    if (tree == nullptr) {
+        return;
+    }
+
+    leftSub.push_back(tree->value);
+
+    computeLeftSub(tree->left, leftSub);
+
+    computeLeftSub(tree->right, leftSub); 
+}
+
+void computeRightSub(BinaryTree* tree, vector<int>& rightSub) {
+    if (tree == nullptr) {
+        return;
+    }
+
+    rightSub.push_back(tree->value);
+
+    computeRightSub(tree->right, rightSub);
+
+    computeRightSub(tree->left, rightSub); 
+}
+
 bool symmetricalTree(BinaryTree* tree) {
     // Write your code here.
-    return false;
+    vector<int> leftSub;
+    vector<int> rightSub;
+
+    computeLeftSub(tree->left, leftSub);
+    computeRightSub(tree->right, rightSub);
+
+    if (leftSub.size() != rightSub.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < leftSub.size(); i++) {
+        if (leftSub[i] != rightSub[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 
 void test_0(void) {
     std::cout << "Hello!" << std::endl;
 
-    BinaryTree* tree1 = new BinaryTree(1);
-    tree1->left = new BinaryTree(3);
-    tree1->left->left = new BinaryTree(7);
-    tree1->left->right = new BinaryTree(4);
-    tree1->right = new BinaryTree(2);
+    BinaryTree* tree = new BinaryTree(10);
+    tree->left = new BinaryTree(5);
+    tree->right = new BinaryTree(5);
+    tree->left->left = new BinaryTree(7);
+    tree->left->right = new BinaryTree(9);
+    tree->right->left = new BinaryTree(9);
+    tree->right->right = new BinaryTree(7);
+    auto expected = true;
+    auto actual = symmetricalTree(tree);
+    assert(expected == actual);
+}
 
-    BinaryTree* tree2 = new BinaryTree(1);
-    tree2->left = new BinaryTree(5);
-    tree2->left->left = new BinaryTree(2);
-    tree2->right = new BinaryTree(9);
-    tree2->right->left = new BinaryTree(7);
-    tree2->right->right = new BinaryTree(6);
+void test_13(void) {
+    std::cout << "Hello!" << std::endl;
 
-    auto actual = mergeBinaryTrees(tree1, tree2);
-    assert(actual->value == 2);
-    assert(actual->left->value == 8);
-    assert(actual->left->left->value == 9);
-    assert(actual->left->right->value == 4);
-    assert(actual->right->value == 11);
-    assert(actual->right->left->value == 7);
-    assert(actual->right->right->value == 6);
+    BinaryTree* tree = new BinaryTree(1);
+    tree->left = new BinaryTree(2);
+    tree->right = new BinaryTree(2);
+    tree->left->left = new BinaryTree(3);
+    tree->left->right = new BinaryTree(4);
+    tree->left->right->left = new BinaryTree(5);
+    tree->right->left = new BinaryTree(4);
+    tree->right->right = new BinaryTree(3);
+    tree->right->left->right = new BinaryTree(5);
+    auto expected = true;
+    auto actual = symmetricalTree(tree);
+    assert(expected == actual);
 }
 
 int main()
 {
-    test_0();
+    test_13();
 }
---
+++ algo-expert/03-binary_trees/10-split_binary_tree/split_binary_tree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cassert>
+#include <algorithm>
+#include <stack>
+#include<string>
+
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+    public:
+        int value;
+        BinaryTree* left = nullptr;
+        BinaryTree* right = nullptr;
+
+        BinaryTree(int value) { this->value = value; }
+};
+
+int splitBinaryTree(BinaryTree* tree) {
+    // Write your code here.
+    return -1;
+}
+
+
+void test_0(void) {
+    std::cout << "Hello!" << std::endl;
+
+    BinaryTree* tree1 = new BinaryTree(1);
+    tree1->left = new BinaryTree(3);
+    tree1->left->left = new BinaryTree(7);
+    tree1->left->right = new BinaryTree(4);
+    tree1->right = new BinaryTree(2);
+
+    BinaryTree* tree2 = new BinaryTree(1);
+    tree2->left = new BinaryTree(5);
+    tree2->left->left = new BinaryTree(2);
+    tree2->right = new BinaryTree(9);
+    tree2->right->left = new BinaryTree(7);
+    tree2->right->right = new BinaryTree(6);
+
+    auto actual = mergeBinaryTrees(tree1, tree2);
+    assert(actual->value == 2);
+    assert(actual->left->value == 8);
+    assert(actual->left->left->value == 9);
+    assert(actual->left->right->value == 4);
+    assert(actual->right->value == 11);
+    assert(actual->right->left->value == 7);
+    assert(actual->right->right->value == 6);
+}
+
+int main()
+{
+    test_0();
+}
